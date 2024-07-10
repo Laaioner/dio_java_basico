31177d03893b47d8278cec78bfe3d8f39baae10c
Pedro <pedropiovesan2002@gmail.com>
2024-07-10 12:33:36 -0300

    Chamando uma função

--- meu_primeiro_projeto_java/src/App.java
+++ meu_primeiro_projeto_java/src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Olá mundo!");
+        Scanner sc = new Scanner(System.in);
+		
+        int A, B, PROD;
+		
+        A = -30;
+        B = 10;
+		
+        // Calcular o produto de A e B
+        PROD = A * B;  
+
+        // Exibir o resultado
+        System.out.println("PROD = " + PROD);
+
+        // Fechar o scanner
+        sc.close();
     }
 }
