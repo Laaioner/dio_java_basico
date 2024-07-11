ebd0dd8424511c93c3049934d44c42b719d2e47b
Pedro <pedropiovesan2002@gmail.com>
2024-07-11 17:30:48 -0300

    Operadores

---
+++ Anatomia_de_classes/src/edu/pedro/Operadores/operadores.java
@@ -0,0 +1,18 @@
+package edu.pedro.Operadores;
+
+public class operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+        
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = + numero; //errado, lembre das regras matemáticas
+        
+        numero = numero * -1;
+
+        System.out.println(numero);
+    }
+    
+}
