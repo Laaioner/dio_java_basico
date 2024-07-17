1d154fcd3549a2babba97d432600b1d65bee71c8
Pedro <pedropiovesan2002@gmail.com>
2024-07-16 22:19:04 -0300

    Realizando o projeto banco

--- Anatomia_de_classes/src/edu/pedro/Terminal_e_argumentos/MinhaClasse.java
+++ Anatomia_de_classes/src/edu/pedro/Terminal_e_argumentos/MinhaClasse.java
@@ -3,5 +3,9 @@ package edu.pedro.Terminal_e_argumentos;
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("Olá, mundo!");
+        /*para executar o comando dentro de um terminal é dessa forma 
+         * java edu.pedro.Terminal_e_argumentos.MinhaClasse
+         * lembrando que esse . é vc acessando as pastas posteriores
+        */
     }
 }
