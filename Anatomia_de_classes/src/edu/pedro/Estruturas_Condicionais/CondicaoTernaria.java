5d3ac501c790ffa381f8d6233ee630b0c613cb2d
Pedro <pedropiovesan2002@gmail.com>
2024-07-19 23:25:11 -0300

    Criação das estruturas condicionais

---
+++ Anatomia_de_classes/src/edu/pedro/Estruturas_Condicionais/CondicaoTernaria.java
@@ -0,0 +1,9 @@
+package edu.pedro.Estruturas_Condicionais;
+
+public class CondicaoTernaria {
+    public static void main(String[] args) {
+        int nota = 5;
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota <7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
